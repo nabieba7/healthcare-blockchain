66adbe6bca09a9d12baaa9b4ca790d26bde36d4d
nabieba7 <nabieba7@gmail.com>
2025-11-19 11:20:47 +0000

    Deployment scripts

---
+++ scripts/deploy.js
@@ -0,0 +1,18 @@
+
+async function main() {
+    const [deployer] = await ethers.getSigners();
+    console.log("Deploying contracts with the account:", deployer.address);
+  
+    const MedicalRecords = await ethers.getContractFactory("MedicalRecords");
+    const medicalRecords = await MedicalRecords.deploy();
+    
+    await medicalRecords.deployed();
+    console.log("MedicalRecords deployed to:", medicalRecords.address);
+  }
+  
+  main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
